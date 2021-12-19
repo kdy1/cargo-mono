4e95092a124ec3a0015f17ae032fdbaa92c229dd
Donny <kdy1997.dev@gmail.com>
2021-12-19 13:02:03 +0900

    Simplify

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ enum Command {
     Publish(PublishCommand),
 }
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
+#[tokio::main]
 async fn main() -> Result<()> {
     let args = {
         let mut args = env::args().collect::<Vec<_>>();
