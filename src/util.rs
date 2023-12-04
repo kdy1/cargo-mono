5a3540a107212c897f14ca941d89fe01772c9629
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-04 16:14:42 +0900

    WIP

--- src/util.rs
+++ src/util.rs
@@ -35,18 +35,38 @@ pub async fn get_published_versions(
 
 /// Fetches the current version from crates.io
 async fn fetch_published_version(name: &str, allow_not_found: bool) -> Result<Version> {
-    let index = crates_index::GitIndex::new_cargo_default()
-        .context("failed to read the git index for crates.io")?;
+    let body = reqwest::get(&build_url(package_name)).await?.text().await?;
 
-    let pkg = match index.crate_(name) {
-        Some(v) => v,
-        None => {
-            if !allow_not_found {
-                bail!("failed to find crate {}", name)
+    let mut v = body
+        .lines()
+        .into_iter()
+        .filter_map(|line| {
+            let desc = serde_json::from_str::<Descriptor>(&line);
+            let line = match desc {
+                Ok(v) => v,
+                Err(err) => return Some(Err(anyhow!("failed to parse line: {:?}\n{}", err, line))),
+            };
+
+            if !constraints.matches(&line.vers) {
+                return None;
             }
-            return Ok(Version::new(0, 0, 0));
-        }
-    };
+
+            Some(Ok(PackageVersion {
+                name: line.name,
+                version: line.vers,
+                deps: line
+                    .deps
+                    .into_iter()
+                    .filter(|dep| dep.kind == "normal")
+                    .map(|d| Dependency {
+                        name: d.package.unwrap_or(d.name),
+                        constraints: d.req,
+                    })
+                    .collect(),
+            }))
+        })
+        .collect::<Result<Vec<_>>>()
+        .with_context(|| format!("failed to parse index of {}", package_name))?;
 
     let v = pkg.highest_version();
     Ok(v.version()
@@ -68,4 +88,22 @@ pub fn can_publish(p: &Package) -> bool {
     }
 
     true
+}
+
+fn build_url(name: &str) -> String {
+    let name = name.to_ascii_lowercase();
+    match name.len() {
+        1 => format!("https://index.crates.io/1/{name}"),
+        2 => format!("https://index.crates.io/2/{name}"),
+        3 => {
+            let first_char = name.chars().next().unwrap();
+            format!("https://index.crates.io/3/{first_char}/{name}")
+        }
+        _ => {
+            let first_two = &name[0..2];
+            let second_two = &name[2..4];
+
+            format!("https://index.crates.io/{first_two}/{second_two}/{name}",)
+        }
+    }
 }
